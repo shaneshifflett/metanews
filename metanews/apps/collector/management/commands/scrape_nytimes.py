27f45359be29eb7943cc7d10b3ba62aa039791d7
Shane Shifflett <shifflett.shane@gmail.com>
2013-08-01 16:43:35 -0400

    fix table, rm cruft

--- metanews/apps/collector/management/commands/scrape_nytimes.py
+++ metanews/apps/collector/management/commands/scrape_nytimes.py
@@ -22,37 +22,6 @@ def clean_link(url):
     url = url.split("?")[0]
     return url
 
-def do_work(links, place):
-    for link in links:
-        url = clean_link(link['href'])
-        if len(url.split("http://")) > 1:
-            fn = slugify(url)
-            print "getting url = %s" % url
-            try:
-                with open("data/scrapes/nytimes/"+fn, "rb") as f:
-                    content = f.read()
-            except IOError:
-                page = requests.get(url)
-                with open("data/scrapes/nytimes/"+fn, "wb") as f:
-                    f.write(page.content)
-                    content = page.content
-            pagesoup = BeautifulSoup(content)
-            spans = pagesoup.findAll("span", "color_1A1A1A")
-            if len(spans) == 1:
-                name = spans[0].text.split("By ")[1]
-                if len(name.split(' and ')) > 1:
-                    for nm in name.split(' and '):
-                        nslug = slugify(nm)
-                        print nslug
-                        au, created = Author.objects.get_or_create(organization=org, slug=nslug, name=nm)
-                        au.save()
-                        create_copy(au, url, content, place)
-                else:
-                    nslug = slugify(name)
-                    print nslug
-                    au, created = Author.objects.get_or_create(organization=org, slug=nslug, name=name)
-                    create_copy(au, url, content, place)
-
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
