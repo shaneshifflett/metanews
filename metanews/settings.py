a5dc70f5968239bbf61742ac683a6c4cafff2344
Shane Shifflett <shifflett.shane@gmail.com>
2013-08-01 13:39:44 -0400

    db change

--- metanews/settings.py
+++ metanews/settings.py
@@ -18,9 +18,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'metanews',                      # Or path to database file if using sqlite3.
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'USER': 'jayboice',
+        'PASSWORD': 'WhoUvote4',
+        'HOST': 'vm-149-174-151-38.asset.aol.com',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
 }
